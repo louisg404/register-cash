3d525e0cac1a8d7fc18bb55e7715ed3f5c531ea4
louisg404 <louis.gautier404@gmail.com>
2021-03-31 16:38:34 +0200

    feat/ Afficher tableau pays

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print('Hello World')
+class pays(object):
+    def __init__(self, pays, code, tva):
+        self.pays = pays
+        self.code = code
+        self.tva = tva
+
+codesPays = []
+codesPays.append(pays('France', 'FR', 20))
+codesPays.append(pays('Espagne', 'ES', 21))
+codesPays.append(pays('Italie', 'IT', 22))
+codesPays.append(pays('Luxembourg', 'LU', 17))
+codesPays.append(pays('Portugal', 'PT', 23))
+codesPays.append(pays('Roumanie', 'RO', 19))
+
+def afficherTableau():
+    print("-------------------------------")
+    for item in codesPays:
+        print item.pays, "(" + item.code + ")", "TVA :", item.tva
+    print("-------------------------------")
+
+# Demarrage
+afficherTableau()
